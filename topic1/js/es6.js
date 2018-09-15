235e7ca51b390278fa04eef5f5bc100439e5f5e7
sofyaiello1991 <sofia.aiello1991@gmail.com>
2018-09-15 17:09:00 -0300

    comentario

--- topic1/js/es6.js
+++ topic1/js/es6.js
@@ -39,6 +39,53 @@ function httpAjax(config){
     };
   	if ("withCredentials" in request) {
 	    request.open(config.method, config.url, true);
+      request.setRequestHeader('Content-Type', 'text/plain');
+	} else if (typeof XDomainRequest != "undefined") {
+	    request = new XDomainRequest();
+      request.open(config.method, config.url);
+	    request.setRequestHeader('Content-Type', 'text/plain');
+	} else {
+	    request = null;
+	}
+    request.send();
+  });
+};
+
+let ajax = document.getElementById('ajax');
+httpAjax({
+	url:'http://api.icndb.com/jokes/random',
+	method:'GET'
+})
+.then(function(value){
+  console.log("success");
+  ajax.className += "success";
+})
+.catch(function(reason){
+  console.log("error ", reason);
+  ajax.className += "error";
+})
+
+/*Cree una función para obtener la respuesta de 
+https://api.github.com/search/repositories 
+con parámetros q = 'JavaScript'.*/
+function httpRepo(config){
+  return new Promise(function(resolve, reject){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(request.readyState == 4){
+        if(request.status === 200){
+          resolve(request.response);
+        }
+        else{
+          reject(new Error(request.statusText));
+        }
+      }
+    };
+    request.onerror = function(){
+      reject(new Error(this.statusText));
+    };
+  	if ("withCredentials" in request) {
+	    request.open(config.method, config.url, true);
 	    request.setRequestHeader('Content-Type', 'text/plain');
 	} else if (typeof XDomainRequest != "undefined") {
 	    request = new XDomainRequest();
@@ -51,16 +98,27 @@ function httpAjax(config){
   });
 };
 
-let ajax = document.getElementById('ajax');
-httpAjax({
-	url:' http://api.icndb.com/jokes/random',
-	method:'get'
+let repo = document.getElementById('resprepo');
+httpRepo({
+	url:'https://api.github.com/search/repositories?q="+Javascript',
+	method:'GET'
 })
 .then(function(value){
-  console.log("success");
-  ajax.className += "success";
+    console.log(value);
+
+    for (i = 0; i < 50; i++){
+      console.log(value[i]);
+      var z = document.createElement('li');
+      z.className += "right"; 
+      z.innerHTML=value[i];
+
+      document.body.appendChild(z);
+
+    }
+    repo.className += "success";
+    console.log("success");
 })
 .catch(function(reason){
   console.log("error ", reason);
-  ajax.className += "error";
+  repo.className += "error";
 })
